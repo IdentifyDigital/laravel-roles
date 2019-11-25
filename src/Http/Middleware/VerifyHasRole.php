effb4743983fb39836a2af7ae85476a8033abf48
Josh Thackeray <josh@identifywebdesign.co.uk>
2019-11-25 17:09:09 +0000

    Added VerifyHasRole middleware

---
+++ src/Http/Middleware/VerifyHasRole.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+
+class VerifyHasRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @param array $roles
+     * @return void
+     *
+     * @throws AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        if($this->validate($request, $roles))
+            return $next($request);
+
+        throw new AuthenticationException(
+            'Unauthenticated.', [], $this->redirectTo($request)
+        );
+    }
+
+    /**
+     * Validates the given user has one of the specified roles.
+     *
+     * @param Request $request
+     * @param array $roles
+     * @return bool
+     */
+    private function validate(Request $request, array $roles)
+    {
+        $user = $request->user();
+
+        if(!method_exists($user, 'hasRole'))
+           return false;
+
+        foreach($roles as $role) {
+            if($user->hasRole($role))
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the path the user should be redirected to when they are not assigned to any of the roles.
+     *
+     * @param Request $request
+     * @return string
+     */
+    protected function redirectTo($request)
+    {
+        return route('login');
+    }
+}
